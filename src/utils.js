99e124525b2a7fbfd470f60ec376ffda96051155
pwangy <blipsandclicks@gmail.com>
2024-03-03 16:30:50 -0800

    complete codealong

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
-// Your code here
+export const currentAgeForBirthYear = (birthYear) => {
+    const currentYear = 2022 //as per codealong
+    const currentAge = currentYear - birthYear
+
+    return currentAge
+}
+
+// console.log(currentAgeForBirthYear(1984))
